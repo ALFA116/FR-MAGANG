a17652d9f0e316e75ac1b1a2faee6d9fb98a7ecc
ALFA <160485300+ALFA116@users.noreply.github.com>
2024-10-18 14:10:17 +0700

    first init

---
+++ pages/index.php
@@ -0,0 +1,407 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FR MAGANG SMKN64 WEB</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <div class="home-pri">
+    <!-- /* tampilan home */ -->
+    <section id="home" class="home">
+      <div class="container-1">
+        <div class="container-8 d-flex justify-content-between align-items-center">
+          <div class="image-9"></div>
+          <div class="container-6 d-flex justify-content-center align-items-center">
+            <div class="home-1 navbar-item"><a href="#home" class="active">Home</a></div>
+            <div class="about navbar-item"><a href="#our-journey">About</a></div>
+            <div class="work navbar-item"><a href="#work">Work</a></div>
+            <div class="product-solutions navbar-item dropdown">
+              <a href="#" class="dropbtn">Product &amp; Solutions <span class="arrow-down">▼</span></a>
+              <div class="dropdown-content">
+                <a href="ip-telephony-solution.php">IP Telephony Solution</a>
+                <a href="network-connectivity.php">Network &amp; Connectivity Solution</a>
+                <a href="design-testing.php">Design &amp; Testing</a>
+                <a href="other-services.php">Other IT Services</a>
+              </div>
+            </div>
+            <div class="magang navbar-item"><a href="#magang">Magang</a></div>
+            <div class="contact navbar-item"><a href="#contact">Contact</a></div>
+          </div>
+        </div>
+        <span class="your-consultancy-partner-for-growth">
+          Your Consultancy<br>
+          Partner for Growth.
+        </span>
+        <div class="find-solutions-and-work-fast-implementing-idea-let-us-manage-your-it-infrastructure-stop-doing-works-that-arent-your-core-businesses">
+          Find Solutions and Work Fast implementing Idea. Let us<br>
+          Manage Your IT Infrastructure Stop doing works that aren&#39;t<br>
+          your core businesses.
+        </div>
+        <div class="group-88 d-flex justify-content-between align-items-center">
+          <div class="contohgmail-com">Contoh@gmail.com</div>
+          <div class="group-86 d-flex justify-content-center align-items-center">
+            <span class="email-1">Email</span>
+          </div>
+        </div>
+        <div class="container-9 d-flex justify-content-between">
+          <div class="group-83 d-flex">
+            <img class="group-81" src="../assets/vectors/group_81_x2.svg" />
+            <img class="group-82" src="../assets/vectors/group_82_x2.svg" />
+          </div>
+          <div class="group-84 d-flex justify-content-center align-items-center">
+            <div class="frame-14">
+              <img class="vector-15" src="../assets/vectors/vector_4_x2.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Tampilan Our Journey -->
+    <section id="our-journey" class="home">
+      <div class="container-2">
+        <div class="d-flex flex-row flex-wrap">
+          <div class="text-column" style="margin-right: 20px; padding-left: 20px;">
+            <h2 class="text-wrapper-14">Our Journey</h2>
+            <p class="text-wrapper-15">Reasons to choose Our<br>
+              Solution | Already<br>
+              collaboration with many<br>
+              clients</p>
+          </div>
+          <div class="image-column">
+            <div class="row">
+              <div class="col-md-2 col-6 mb-4">
+                <img class="img-fluid" src="../assets/img/rectangle-22.png" alt="JAKARTA" />
+                <div class="text-wrapper-16">JAKARTA</div>
+              </div>
+              <div class="col-md-2 col-6 mb-4">
+                <img class="img-fluid" src="../assets/img/rectangle-25.png" alt="BEKASI" />
+                <div class="text-wrapper-16">BEKASI</div>
+              </div>
+              <div class="col-md-2 col-6 mb-4">
+                <img class="img-fluid" src="../assets/img/image-1.png" alt="DEPOK" />
+                <div class="text-wrapper-16">DEPOK</div>
+              </div>
+              <div class="col-md-2 col-6 mb-4">
+                <img class="img-fluid" src="../assets/img/image-2.png" alt="BOGOR" />
+                <div class="text-wrapper-16">BOGOR</div>
+              </div>
+              <div class="col-md-2 col-6 mb-4">
+                <img class="img-fluid" src="../assets/img/image-3.png" alt="BANDUNG" />
+                <div class="text-wrapper-16">BANDUNG</div>
+              </div>
+              <div class="col-md-2 col-6 mb-4">
+                <img class="img-fluid" src="../assets/img/image-4.png" alt="KARAWANG" />
+                <div class="text-wrapper-16">KARAWANG</div>
+              </div>
+              <div class="col-md-2 col-6 mb-4">
+                <img class="img-fluid" src="../assets/img/image-5.png" alt="YOGYAKARTA" />
+                <div class="text-wrapper-16">YOGYAKARTA</div>
+              </div>
+              <div class="col-md-2 col-6 mb-4">
+                <img class="img-fluid" src="../assets/img/image-7.png" alt="SOLO" />
+                <div class="text-wrapper-16">SOLO</div>
+              </div>
+              <div class="col-md-2 col-6 mb-4">
+                <img class="img-fluid" src="../assets/img/image-8.png" alt="SURABAYA" />
+                <div class="text-wrapper-16">SURABAYA</div>
+              </div>
+              <div class="col-md-2 col-6 mb-4">
+                <img class="img-fluid" src="../assets/img/image-9.png" alt="MALANG" />
+                <div class="text-wrapper-16">MALANG</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- Tampilan About Us -->
+    <section id="about-us" class="home">
+      <div class="container-3 frame-21">
+        <div class="row text-center">
+          <div class="col-md-4">
+            <div class="container-3 frame-24">
+              <img class="frame-8" src="../assets/img/frame-5.svg" />
+              <div class="title">IT &amp; Telecom-Savvy Team</div>
+            </div>
+            <p class="text-description">
+              We are experienced professionals that have<br>
+              served IT and telecom industries for more than twelve years.
+              We bring a combination of technical and market knowledge to each of our projects.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <div class="container-3 frame-24">
+              <img class="frame-8" src="../assets/img/frame-6.svg" />
+              <div class="title">Halo Collaboration</div>
+            </div>
+            <p class="text-description">
+              Collaborate with multinational companies, which have expertise in IT solution and telecom carriers, we
+              tailor and match customers' needs with effective and efficient solutions.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <div class="container-3 frame-24">
+              <img class="frame-8" src="../assets/img/frame-7.svg" />
+              <div class="title">Value to Customers</div>
+            </div>
+            <p class="text-description">
+              By focusing solely on areas where we excel, we aim to provide a first-class service to our customers and
+              bring value for our customers by producing measurable results and cost savings.
+            </p>
+          </div>
+        </div>
+        <div class="text-center mt-4">
+          <div class="container-3 group-5">
+            <div class="container-3 overlap-group-4">
+              <div class="text-wrapper-20">
+                <a href="#" style="color:red" class="about-button" id="aboutUsButton">About Us</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Modal Structure -->
+    <div id="aboutUsModal" class="modal">
+      <div class="modal-content">
+        <span class="close-button" id="closeModal">&times;</span>
+        <h2>About Us</h2>
+        <p>PT. Meissa Berkah Teknologi (www.first-routes.com) is a company that provides Information and communications technology (ICT) solutions under the brand First Routes. The company is established in 2014 by a team of energetic and experienced telecommunication professionals that aim to deliver effective and efficient telecommunication services to every single customer.</p>
+        <p>Our team of sales and marketing, CRM, technical and IT departments are all experienced professionals that have served ICT industry for more than ten years. We bring a combination of technical and market knowledge to each of our projects.</p>
+        <p>Collaborate with international and local companies, which have expertise in IT solution, and Indonesian mobile operators that provide quality service and competitive price, we tailor and match customers' needs with effective and efficient solution.</p>
+        <p>By focusing solely on areas where we excel, we aim to provide a first-class service to our customers and bring the value for our partners by producing both measurable results and cost savings.</p>
+
+        <!-- Google Maps Iframe -->
+        <div class="map-container">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.2920441526567!2d106.778863!3d-6.35623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1a0d5396aa5%3A0xfcc932656de029fc!2sPT%20Meissa%20Berkah%20technology!5e0!3m2!1sid!2sid!4v1727943808185!5m2!1sid!2sid" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+    </div>
+
+    <!-- TAMPILAN WORK WITH US -->
+    <section id="work" class="work-section">
+      <div class="work-container">
+        <div class="work-text">
+          <div class="text-wrapper-21">WORK WITH US</div>
+          <div class="collaboration-for">Collaboration <br />for New Invention</div>
+          <p class="text-wrapper-22">
+            What We Do<br />PT. Meissa Berkah Teknologi is a specialist company<br>
+            that delivers telecommunication services under the brand First<br>
+            Routes.The company is established in 2014 by a team of energetic and
+            experienced telecoms professionals that aim to deliver effective and efficient telecommunication
+            services.
+          </p>
+        </div>
+        <div class="work-assessment">
+          <div class="assessment-card">
+            <img class="image-2" src="../assets/img/image-10.png" />
+            <div class="text-wrapper-24">UI/UX Design</div>
+            <p class="text-wrapper-25">
+              Antarmuka sangat intuitif dan mudah dipahami oleh pengguna baru. Tugas-tugas utama dapat
+              diselesaikan dengan cepat dan efisien.
+            </p>
+            <div class="assessment-info">
+              <div class="text-wrapper-23">[ Best Service ]</div>
+              <img class="frame-33" src="../assets/img/frame-119.svg" />
+            </div>
+            <div class="frame-24">
+              <div class="group-6"><img class="frame-35" src="../assets/img/frame-8.svg" /></div>
+              <div class="group-7"><img class="frame-35" src="../assets/img/frame-9.svg" /></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="work-stats">
+        <div class="stat-item">
+          <img class="image-3" src="../assets/img/image-11.png" />
+          <div class="stat-info">
+            <div class="text-wrapper-24">12+</div>
+            <p class="text-wrapper-26">Telco Carriers and ISP Partners</p>
+          </div>
+        </div>
+        <div class="stat-item">
+          <img class="image-3" src="../assets/img/image-12.png" />
+          <div class="stat-info">
+            <div class="text-wrapper-24">120+</div>
+            <p class="text-wrapper-26">Project Done</p>
+          </div>
+        </div>
+        <div class="stat-item">
+          <img class="image-3" src="../assets/img/image-13.png" />
+          <div class="stat-info">
+            <div class="text-wrapper-24">90+</div>
+            <p class="text-wrapper-26">Corporate Clients</p>
+          </div>
+        </div>
+        <div class="stat-item">
+          <img class="image-3" src="../assets/img/image-14.png" />
+          <div class="stat-info">
+            <div class="text-wrapper-24">0+</div>
+            <p class="text-wrapper-26">Pending Project</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Tampilan Our Contact -->
+    <section id="contact" class="home">
+      <div class="frame">
+        <div class="frame-2">
+          <div class="frame-3">
+            <div class="text-wrapper">OUR CONTACT</div>
+            <p class="get-schedule-with-us">
+              <span class="span">Get Schedule</span> <span class="text-wrapper-2"> with Us.</span>
+            </p>
+          </div>
+          <div class="frame-4">
+            <div class="frame-5">
+              <div class="group"><img class="img" src="../assets/img/frame.svg" /></div>
+              <p class="p">Jl. Guntur No.20C, Limo, Kec. Limo, Kota Depok, Jawa Barat 16514</p>
+            </div>
+            <div class="frame-5">
+              <div class="frame-wrapper"><img class="img" src="../assets/img/frame-1.svg" /></div>
+              <div class="text-wrapper-3">Sales Representative</div>
+            </div>
+          </div>
+        </div>
+        <div class="frame-6">
+          <div class="frame-7">
+            <img class="frame-8" src="../assets/img/frame-2.svg" />
+            <p class="leave-a-message">
+              <span class="span">Leave </span> <span class="text-wrapper-4">A Message.</span>
+            </p>
+          </div>
+          <div class="frame-9">
+            <div class="frame-6">
+              <div class="frame-10">
+                <div class="frame-11">
+                  <div class="text-wrapper-5">Name</div>
+                  <div class="overlap-group-wrapper">
+                    <div class="overlap-group">
+                      <div class="text-wrapper-6">Full Name</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="frame-11">
+                  <div class="text-wrapper-5">Email</div>
+                  <div class="overlap-group-wrapper">
+                    <div class="overlap-group">
+                      <div class="text-wrapper-7">Enter Your Email</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="frame-12">
+                <div class="text-wrapper-5">Your Message</div>
+                <div class="div-wrapper">
+                  <div class="overlap-group-2">
+                    <div class="text-wrapper-8">Enter Your Message...</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="frame-13">
+              <div class="text-wrapper-9">Send Now</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- Tampilan Footer -->
+    <section>
+    <div class="footer">
+      <div class="footer-content">
+        <img class="image-4" src="../assets/img/image-6-1.png" alt="Logo" />
+        <div class="footer-info">
+          <p class="we-provide-a-range">
+            We provide a range of IT and telecommunication services consisting of telecomm consultancy, SIP Trunk
+            provider, Call Center Solution, LAN Setup.<br />We do the concept, technical design, and implementation
+            that fits customers&#39; need both effective cost-efficient.
+          </p>
+          <div class="footer-links">
+            <div class="footer-section">
+              <div class="text-wrapper-27">Service</div>
+              <div class="frame-6">
+                <div class="text-wrapper-28">Our Work</div>
+                <div class="text-wrapper-29">Our Solutions</div>
+              </div>
+            </div>
+            <div class="footer-section">
+              <div class="text-wrapper-27">About Us</div>
+              <div class="frame-6">
+                <div class="text-wrapper-28">Overview</div>
+                <div class="text-wrapper-29">About</div>
+                <div class="text-wrapper-29">Awards &amp; Recognitions</div>
+                <div class="text-wrapper-29">Contact Team</div>
+              </div>
+            </div>
+            <div class="footer-section">
+              <div class="text-wrapper-27">PT. Meissa Berkah Teknologi</div>
+              <div class="frame-6">
+                <p class="text-wrapper-30">Jl. Guntur No.20C, Limo, Kec. Limo, Kota Depok, Jawa Barat 16514</p>
+                <div class="text-wrapper-22">+6285972510714</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- TAMPILAN FOOTER MENGAMBANG -->
+      <img class="group-8" src="../assets/img/group-78.png" alt="Group" />
+      <div class="footer-bottom">
+        <p class="text-wrapper-31">Copyright@2024. First-Routes Web Team All Rights Reserved.</p>
+        <div class="social-icons">
+          <img class="frame-46" src="../assets/img/frame-10.svg" alt="Icon 1" />
+          <img class="frame-46" src="../assets/img/frame-11.svg" alt="Icon 2" />
+          <img class="frame-46" src="../assets/img/frame-12.svg" alt="Icon 3" />
+          <img class="frame-46" src="../assets/img/frame-13.svg" alt="Icon 4" />
+        </div>
+        <div class="text-wrapper-32">Akun media sosial kami</div>
+      </div>
+    </div>
+    </section>
+
+
+    <script>
+      // Get modal element
+      const modal = document.getElementById("aboutUsModal");
+      const aboutUsButton = document.getElementById("aboutUsButton");
+      const closeModalButton = document.getElementById("closeModal");
+
+      // Listen for open click
+      aboutUsButton.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent default anchor click behavior
+        modal.style.display = "block"; // Show the modal
+      });
+
+      // Listen for close click
+      closeModalButton.addEventListener("click", function() {
+        modal.style.display = "none"; // Hide the modal
+      });
+
+      // Listen for outside click
+      window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none"; // Hide the modal if clicked outside
+        }
+      });
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
